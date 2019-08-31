9eadbfbe99b31e3111b36aefa25f9f3657d1bc3b
vanbumi <jsp.dio@gmail.com>
2019-08-31 17:13:48 +0700

    02 done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// create
+// create static page
+app.use(express.static('public'));
 
-
+// create route pages > index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/index.html'));
+});
 
 
 
