41d1b4ccfa97842ebd8c9b244838b276a0767bd9
vanbumi <jsp.dio@gmail.com>
2019-08-31 16:33:46 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+// create
+
+
+
+
+
+
+
+
+
+
+// start server
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`)
+}); 
