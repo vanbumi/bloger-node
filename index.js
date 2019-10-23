04252e7b3531d4bff76c48916904344f5d5cdeef
vanbumi <jsp.dio@gmail.com>
2019-10-23 10:10:29 +0700

    materi bagian 06 done

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const path = require('path');
 const { config, engine } = require('express-edge');
+const mongoose = require('mongoose');
+const Post = require('./database/models/Post');
+const bodyParser = require('body-parser');
+
+// konek database atlas
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dyo:dyo123@bloggernode-udx5f.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+client.connect(err => {
+  if (!err) {
+    console.log('Database mongodb CONNECTED!')
+  }
+
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const app = express();
+
+// setup body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // create static page
 app.use(express.static('public'));
@@ -11,7 +35,7 @@ app.use(express.static('public'));
 app.use(engine);
 app.set('views', `${__dirname}/views`);
 
-// create route pages > index.html
+// create route pages > index
 app.get('/', (req, res) => {
   //res.sendFile(path.join(__dirname, 'pages/index.html'));
   res.render('index', {
@@ -19,22 +43,50 @@ app.get('/', (req, res) => {
   });
 });
 
-// create route pages > about.html
+// create route pages > about
 app.get('/about', (req, res) => {
   //res.sendFile(path.resolve(__dirname, "pages/about.html"));
   res.render('about');
 });
 
-// create route pages > post.html
+// create route pages > post
 app.get('/post', (req, res) => {
   //res.sendFile(path.resolve(__dirname, "pages/post.html"));
   res.render('post');
 });
 
-// create route pages > contact.html
+// create route pages > contact
 app.get('/contact', (req, res) => {
   //res.sendFile(path.resolve(__dirname, "pages/contact.html"));
   res.render('contact');
+});
+
+// route pages > create
+app.get('/posts/new', (req, res) => {
+  res.render('create')
+});
+
+// route post method POST
+app.post('/posts/store', (req, res) => {
+  // console.log(req.body);
+  // res.send('cobain post');
+
+  Post.create(req.body, (error, post) => {
+    res.redirect('/');
+  });
+
+  // const { title, description, content } = req.body;
+
+  // let post = new Post({
+  //   title,
+  //   description,
+  //   content
+  // });
+
+  // post.save();
+  // res.json(post);
+
+
 });
 
 
